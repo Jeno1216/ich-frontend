d0387646089bcfb4049cb6892b6691eb00ee6317
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-09-13 14:52:16 +0800

    asdada

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { ToastContainer } from 'react-toastify'; // Import ToastContainer
 import 'react-toastify/dist/ReactToastify.css'; // Import the default styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <App />
     <ToastContainer /> {/* <ToastContainer /> put in other pages */}
-  </>,
+  </React.StrictMode>,
 )
