f948bc29696f7a0333f6c0966e6371fd3f08df1b
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-09-13 16:47:51 +0800

    asdads

--- src/People.jsx
+++ src/People.jsx
@@ -31,12 +31,14 @@ function People() {
         
             {users.map(user => (
                 <Link to={`/people/${user._id}`}  className='people border rounded mt-3 d-flex gap-2 p-2 align-items-center text-decoration-none' style={{backdropFilter: 'blur(10px)'}}>
-                    <div className='border rounded-circle' style={{height: '40px', width: '40px'}}>
-                        <img className='element-tilt rounded-circle' src={`https://iloilo-coffee-house-api.onrender.com/Images/${user.file}`} alt="Image" style={{width: '100%', objectFit: 'cover'}}
-                        onError={(e) => {
-                            e.target.src = '/login-image.jpg';
-                        }} />
-                    </div>
+<div className='border rounded-circle' style={{height: '40px', width: '40px', overflow: 'hidden'}}>
+    <img className='element-tilt rounded-circle' src={`https://iloilo-coffee-house-api.onrender.com/Images/${user.file}`} alt="Image" 
+        style={{width: '100%', height: '100%', objectFit: 'cover'}}
+        onError={(e) => {
+            e.target.src = '/login-image.jpg';
+        }} 
+    />
+</div>
 
                     <div className=' d-flex justify-content-between w-100'>
                     <p className=' people-name m-0' style={{fontWeight: '600'}} key={user._id}>{user.username}</p>
