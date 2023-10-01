7f412c76f9807452c039d05b63eb52f258e55278
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-10-01 15:07:01 +0800

    asdsa

--- src/Qwerty.jsx
+++ src/Qwerty.jsx
@@ -1,30 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const Qwerty = () => {
   const [data, setData] = useState(null);
+  const url = 'https://iloilo-coffee-house-api.onrender.com/some-route';
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://iloilo-coffee-house-api.onrender.com/some-route');
-        setData(response.data);
-        console.log("luh gago:", response.data)
-        
-      } catch (error) {
-        console.error('An error occurred while fetching the data:', error);
-      }
-    };
-  
-    fetchData();
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonData => setData(jsonData))
+      .catch(error => console.error('Error fetching data: ', error));
   }, []);
-  
 
   return (
     <div>
       {data ? (
         <div>
-          <h2>Data:</h2>
+          <h2>Data fetched from API:</h2>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
       ) : (
